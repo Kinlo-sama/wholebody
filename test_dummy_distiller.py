ab14df0afea4a4e87c3716f80965356d4246bfab
Kinlo-sama <leohola43@gmail.com>
2026-09-07 03:24:21 +0900

    thirdTryDWm

--- test_dummy_distiller.py
+++ test_dummy_distiller.py
@@ -17,7 +17,7 @@ student_cfg = dict(
         type='RTMCCHead', in_channels=512, out_channels=133, input_size=[256, 192],
         in_featuremap_size=[8, 6], simcc_split_ratio=2.0, final_layer_kernel_size=7,
         gau_cfg=dict(hidden_dims=256, s=128, expansion_factor=2, dropout_rate=0.0, drop_path=0.0, act_fn='SiLU', use_rel_bias=False, pos_enc=False),
-        codec=dict(type='SimCCLabel', input_size=[256, 192], sigma=[4.9, 5.66], simcc_split_ratio=2.0, normalize=False, use_dark=False)
+        codec=dict(type='SimCCLabel', input_size=[256, 192], sigma=[4.9, 5.66], simcc_split_ratio=2.0, normalize=False)
     )
 )
 
@@ -33,7 +33,7 @@ teacher_cfg = dict(
         type='RTMCCHead', in_channels=1024, out_channels=133, input_size=[256, 192],
         in_featuremap_size=[8, 6], simcc_split_ratio=2.0, final_layer_kernel_size=7,
         gau_cfg=dict(hidden_dims=256, s=128, expansion_factor=2, dropout_rate=0.0, drop_path=0.0, act_fn='SiLU', use_rel_bias=False, pos_enc=False),
-        codec=dict(type='SimCCLabel', input_size=[256, 192], sigma=[4.9, 5.66], simcc_split_ratio=2.0, normalize=False, use_dark=False)
+        codec=dict(type='SimCCLabel', input_size=[256, 192], sigma=[4.9, 5.66], simcc_split_ratio=2.0, normalize=False)
     )
 )
 

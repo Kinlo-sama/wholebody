c5a44888fac9c7fec897679090efee3500bcf8c6
Kinlo-sama <leohola43@gmail.com>
2026-09-07 08:19:07 +0900

    fixBackbones

--- wholebody/models/backbones/__init__.py
+++ wholebody/models/backbones/__init__.py
@@ -1,11 +1,13 @@
 from wholebody.models.backbones.base import BaseBackbone
 from wholebody.models.backbones.simple_cnn import SimpleCNN
 from wholebody.models.backbones.resnet import ResNetBackbone
+from .vit_moe import ViTMoE
+from .cspnext import CSPNeXt
 
 __all__ = [
     "BaseBackbone",
     "SimpleCNN",
-    "ResNetBackbone"
+    "ResNetBackbone",
+    "ViTMoE",
+    "CSPNeXt",
 ]
-from .cspnext import CSPNeXt
-__all__.append("CSPNeXt")
